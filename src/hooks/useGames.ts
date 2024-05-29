10ed609e99fd4dc8923a636c54ed157752fd4c41
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-05-30 00:33:18 +0100

    added skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,17 +23,21 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     // Create an instance of AbortController to cancel the request if needed
     const controller = new AbortController();
 
     // Make the API call to fetch games
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => {
         // On success, update the games state with the fetched data
+
         setGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         // Ignore errors from canceled requests
@@ -41,6 +45,7 @@ const useGames = () => {
 
         // For other errors, update the error state
         setError(err.message);
+        setLoading(false);
       });
 
     // Cleanup function to abort the fetch request if the component unmounts
@@ -48,7 +53,7 @@ const useGames = () => {
   }, []);
 
   // Return the games data and any error encountered
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
