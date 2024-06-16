50b3625c049c9b17fde08e0804d38a9a7164ed14
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-06-16 02:21:24 +0100

    new changes

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://server-proxy-9eoa.onrender.com/api",
+  baseURL: "https://server-proxy-9eoa.onrender.com",
   params: {
     key: "0d8ffd519baa41a38f43d72057e2b3a4",
   },
