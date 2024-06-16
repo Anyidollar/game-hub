5645bf5b219dac6de5eafcfa6c308940386711d2
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-06-16 03:29:18 +0100

    new changes

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://server-proxy-9eoa.onrender.com",
+  baseURL: "https://api.rawg.io/api",
   params: {
     key: "0d8ffd519baa41a38f43d72057e2b3a4",
   },
